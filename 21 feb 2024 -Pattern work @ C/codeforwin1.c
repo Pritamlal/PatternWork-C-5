b33911ccb42b04867811b9f98d730fe5eb81ca53
~Pritam~ <126871739+Pritamlal@users.noreply.github.com>
2024-02-21 23:02:49 +0530

    Add files via upload

---
+++ 21 feb 2024 -Pattern work @ C/codeforwin1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("Enter the number::");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			printf("1 ");
+			
+		}
+		printf("\n");
+	}
+}
